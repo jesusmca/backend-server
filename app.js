ae9fadc8785e780e07877553c44c2ad487585fb6
Jesus Moreno <jesusmca@gmail.com>
2020-07-09 17:21:25 -0500

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 // Requires
 var express = require('express');
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 
 
 //Inicializar variables
 var app = express();
+
+// Body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+
+// Importar rutas 
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
+
 
 // Conexion a la base de datos
 mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
@@ -15,16 +28,10 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
     console.log('Base de datos: \x1b[32m%s\x1b[0m', 'online!');
 });
 
-// Rutas
-app.get('/', (req, res, next) => {
-
-    res.status(200).json({
-
-        ok: true,
-        mensaje: 'Petición realizada correctamente.'
-    });
-
-});
+// Rutas - Middleware
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
